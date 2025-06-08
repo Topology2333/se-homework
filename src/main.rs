a9839242b264e02fc4d6ff72de60541dabd51e45
後藤ひとり <118675237+babuzeer@users.noreply.github.com>
2025-06-08 11:55:43 +0800

    Merge pull request #1 from babuzeer/user
    feat: add user login and register

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+mod db;
+mod models;
+mod routes;
+
+use actix_web::{App, HttpServer, web};
+use dotenv::dotenv;
+use actix_cors::Cors;
+use routes::user::user_routes;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    dotenv().ok();
+    let db_pool = db::create_pool().await;
+
+    HttpServer::new(move || {
+        App::new()
+            .wrap(Cors::permissive())
+            .app_data(web::Data::new(db_pool.clone()))
+            .configure(user_routes)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
